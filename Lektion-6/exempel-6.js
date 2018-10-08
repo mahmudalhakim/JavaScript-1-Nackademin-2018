6784f714f591ec37277f51727a4882bb9b24312d
Mahmud Al Hakim <Mahmud.alhakim@gmail.com>
2018-10-08 13:58:03 +0200

    Parametrar och argument

--- Lektion-6/exempel-6.js
+++ Lektion-6/exempel-6.js
@@ -15,12 +15,14 @@ hello(12345);
 hello(true);
 hello();   
 // undefined löser vi med hjälp av 
-// en förvald parameter (default parameter)
+// en förvald parameter (default parameter) . Se rad 2
 
 
 console.log('--------------------------------');
 
+
 // Funktioner med två parametrar
+
 // Skapa en funktion som beräknar summan av två tal
 function sum(x = 0 , y = 0){
     console.log(x+y);
@@ -28,8 +30,22 @@ function sum(x = 0 , y = 0){
 sum(7,9);
 sum();
 sum(123,456,789);
+sum('text');  // text0 (OBS! Konkatenering)
+
 
 console.log('--------------------------------');
+
+// Skapa en funktion som multiplicerar två tal
+function multiply(a=0,b=0){
+    console.log("******  Multiply ****** ");
+    console.log(a*b);
+    console.log("*********************** ");
+
+}
+multiply(10,5);        // 50
+multiply(100);         // 0  (100*0)
+multiply('TEXT', 50);  // NaN
+multiply();            // 0 p.g.a. förvalda parametrar
 
 
 // Skapa en funktion med två parametrar, förnamn och efternamn.
@@ -40,3 +56,46 @@ function hello2(firstName ='Förnamn', lastName='Efternamn'){
 hello2('Mahmud', 'Al Hakim');
 hello2('Kalle', 'Anka');
 hello2();
+
+
+// En egen log-funktion
+function log(text){
+    console.log('************************');
+    console.log('* ' + text);
+    console.log('************************');
+    console.log();
+}
+log('Hej Mahmud');
+
+// En funktion som anropar en annan funktion
+// Funktionen hello3 nedan anropar funktionen log ovan (se rad 62)
+function hello3(name){
+    log('Hello ' + name);
+}
+hello3('Mahmud');
+hello3('Kalle');
+hello3('Erik');
+
+// Funktioner med tre parametrar
+function age(firstName, lastName, age = 'alldeles'){
+
+    log(firstName + ' är ' + age + ' år gammal!');
+    
+    // Eller bättre utskrifter med "Template Strings"
+    log(` ${firstName} är ${age} år gammal! `); // OBS! Backticks 
+
+}
+age('Mahmud', 'Al Hakim' , 45);
+age('Kalle' , 'Anka' , 10);
+age('Mahmud');
+
+// En funktion med ett godtyckligt antal parametrar
+function manyThings(...things){   // ... skapar en array
+    console.log(things);
+}
+manyThings(1);
+manyThings(1,2);
+manyThings(1,2,3);
+manyThings("Mahmud", "Kalle" , "Erik");
+manyThings("Mahmud", "Al Hakim", 45);
+manyThings();
