b65b6f9708396d702224b79b67b2918b2a0dba83
Mahmud Al Hakim <Mahmud.alhakim@gmail.com>
2018-11-01 15:08:25 +0100

    Lektion 13

---
+++ Lektion-13/exempel-4.js
@@ -0,0 +1,8 @@
+function hideElementById(id){
+    console.log("Inne i hideElementById");
+    document.getElementById(id).style.display='none';
+}
+function showElementById(id){
+    console.log("Inne i showElementById");
+    document.getElementById(id).style.display='block'
+}
